93443707f17a9739be1b8dfa9464dd61ef138e06
frederik-h <frederik-h@users.noreply.github.com>
2018-11-20 12:37:34 +0100

    Make all layer keys one shot
    Further change:
    * Use correct modifier for left shift in symbol layer

--- keyboards/redox/keymaps/german-frederik-h/keymap.c
+++ keyboards/redox/keymaps/german-frederik-h/keymap.c
@@ -47,9 +47,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|----+----+----+----+----+----+----|                                                         |----+----+----+----+----+----+----|
      KC_ESC , DE_Q  , DE_W  , DE_E  , DE_R  , DE_T  , KC_ESC,                                       DE_SLSH, DE_Z  , DE_U  , DE_I  , DE_O  , DE_P  , DE_UE,
   //|----+----+----+----+----+----+----|                                                         |----+----+----+----+----+----+----|
-     KC_TAB , DE_A  , DE_S  , DE_D  , DE_F  , DE_G  , LT(_NAV, KC_PGDN),                            TT(_NAV) , DE_H  , DE_J  , DE_K  , DE_L  , DE_OE , DE_AE,
+     KC_TAB , DE_A  , DE_S  , DE_D  , DE_F  , DE_G  , OSL(_NAV),                            TT(_NAV) , DE_H  , DE_J  , DE_K  , DE_L  , DE_OE , DE_AE,
   //|----+----+----+----+----+----+----|                                                         |----+----+----+----+----+----+----|
-     TD(SC_O),  DE_Y  , DE_X  , DE_C  , DE_V  , DE_B  ,LT(_SYMB, KC_PGUP),                          OSL(_SYMB) , DE_N  , DE_M  , DE_COMM, DE_DOT , DE_MINS , TD(SC_C),
+     TD(SC_O),  DE_Y  , DE_X  , DE_C  , DE_V  , DE_B  , OSL(_SYMB),                          OSL(_SYMB) , DE_N  , DE_M  , DE_COMM, DE_DOT , DE_MINS , TD(SC_C),
   //|----+----+----+----+----+----+----|                                                         |----+----+----+----+----+----+----|
      KC_LGUI , DE_LESS , DE_HASH , LGUI_T(DE_PLUS), LALT_T(KC_BSPC), KC_SPC , LCTL_T(KC_ENT) ,      RCTL_T(KC_ENT) , KC_SPC, ALGR_T(KC_DEL) , KC_LEFT, KC_DOWN, KC_UP , KC_RGHT
   //`----+----+----+----+----+----+----'                                                         `----+----+----+----+----+----+----'
@@ -73,7 +73,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5  , _______,                             _______, KC_F6  ,  KC_F7  , KC_F8  , KC_F9  , KC_F10 ,  KC_F11,
     _______, DE_EXLM, DE_QST , DE_LCBR, DE_RCBR, DE_PIPE, DE_HASH,                             DE_BSLS, DE_0   ,  DE_1   , DE_2   , DE_3   , XXXXXXX, KC_F12,
     _______, DE_HASH, DE_DLR , DE_LBRC, DE_RBRC, DE_TILD, _______,                             _______, DE_PLUS,  DE_4   , DE_5   , DE_6   , XXXXXXX, XXXXXXX,
-    _______, DE_PERC, DE_CIRC, DE_LPRN, DE_RPRN, DE_ACUT, _______,                             _______, DE_DOT ,  DE_7   , DE_8   , DE_9   , XXXXXXX, _______,
+    KC_LSFT, DE_PERC, DE_CIRC, DE_LPRN, DE_RPRN, DE_ACUT, _______,                             _______, DE_DOT ,  DE_7   , DE_8   , DE_9   , XXXXXXX, KC_RSFT,
     _______, _______, _______, _______, _______, _______, _______,                             _______, _______,  _______, _______, _______, _______, _______
   ),
 
